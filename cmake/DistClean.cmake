77461924f66fba9a129250e92dfe7016b4f24d9d
Thomas Huth <huth@users.berlios.de>
2011-06-25 18:16:30 +0200

    Remove Ballerburg-specific files during distclean

--- cmake/DistClean.cmake
+++ cmake/DistClean.cmake
@@ -4,6 +4,12 @@
 
 if(UNIX)
 	add_custom_target(distclean  COMMENT "Cleaning up for distribution")
+	# Clean up Ballerburg specific files:
+	foreach(CLEAN_FILE  config.h src/ballerburg install_manifest.txt)
+		add_custom_command(TARGET distclean POST_BUILD
+			COMMAND rm -f ${CLEAN_FILE}
+			DEPENDS clean)
+	endforeach(CLEAN_FILE)
 	# Clean up files that can appear at multiple places:
 	foreach(CLEAN_FILE  CMakeFiles CMakeCache.txt '*.a' '*.1.gz'
 			cmake_install.cmake Makefile)
