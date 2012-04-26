4457354a9403afbb30e799a7c7c063cbb6c01d81
Thomas Huth <huth@tuxfamily.org>
2012-04-26 21:53:20 +0200

    Improved distclean target

--- cmake/DistClean.cmake
+++ cmake/DistClean.cmake
@@ -5,13 +5,14 @@
 if(UNIX)
 	add_custom_target(distclean  COMMENT "Cleaning up for distribution")
 	# Clean up Ballerburg specific files:
-	foreach(CLEAN_FILE  config.h src/ballerburg install_manifest.txt)
+	foreach(CLEAN_FILE  config.h src/ballerburg install_manifest.txt
+			po/ballerburg.pot)
 		add_custom_command(TARGET distclean POST_BUILD
 			COMMAND rm -f ${CLEAN_FILE}
 			DEPENDS clean)
 	endforeach(CLEAN_FILE)
 	# Clean up files that can appear at multiple places:
-	foreach(CLEAN_FILE  CMakeFiles CMakeCache.txt '*.a' '*.1.gz'
+	foreach(CLEAN_FILE  CMakeFiles CMakeCache.txt '*.a' '*.1.gz' '*.mo'
 			cmake_install.cmake Makefile)
 		add_custom_command(TARGET distclean POST_BUILD
 			COMMAND find . -depth -name ${CLEAN_FILE} | xargs rm -rf
